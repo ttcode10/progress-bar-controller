c5e6c8d086a8bbcd1ad19fc93ccc32948c2a42d2
richardpan <richard.pan@dominos.com.au>
2023-06-08 14:08:56 +1000

    Enable number adjustment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,23 @@ import { Heading, Button, Dropdown, BarChart } from '../components';
 
 export const Home = () => {
 	const initialProgresses = [
-		{ name: 'Bar #1', value: 140 },
-		{ name: 'Bar #2', value: 50 },
-		{ name: 'Bar #1', value: -75 },
+		{ name: 'Bar #1', value: 0 },
+		{ name: 'Bar #2', value: 0 },
+		{ name: 'Bar #3', value: 0 },
 	];
 	const barNames = initialProgresses.map((progress) => progress.name);
 	const [dropdownSelectedValue, setDropdownSelectedValue] = useState(barNames[0]);
+	const [progresses, setProgresses] = useState(initialProgresses);
 
-	const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+	const handleButtonClick = (value: number) => {
+		setProgresses(
+			progresses.map((progress) =>
+				progress.name === dropdownSelectedValue ? {...progress, value: progress.value + value} : progress
+			)
+		);
+	};
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
 		e.preventDefault();
 		setDropdownSelectedValue(e.target.value);
 	};
@@ -19,13 +28,13 @@ export const Home = () => {
 	return (
 		<PageContainer>
 			<Heading>Progress Bars Demo</Heading>
-			<BarChart bars={initialProgresses} />
+			<BarChart bars={progresses} />
 			<HorizontalWrapper>
 				<Dropdown values={barNames} selected={dropdownSelectedValue} handleSelect={handleSelect} />
-				<Button>-25</Button>
-				<Button>-10</Button>
-				<Button>+10</Button>
-				<Button>+25</Button>
+				<Button onClick={() => handleButtonClick(-25)}>-25</Button>
+				<Button onClick={() => handleButtonClick(-10)}>-10</Button>
+				<Button onClick={() => handleButtonClick(+10)}>+10</Button>
+				<Button onClick={() => handleButtonClick(+25)}>+25</Button>
 			</HorizontalWrapper>
 		</PageContainer>
 	);
